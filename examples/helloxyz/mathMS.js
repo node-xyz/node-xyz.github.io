88e98574e8a007fffb00c7e7f7d8a80e54914229
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-11-15 19:45:52 +0330

    updated site

---
+++ examples/helloxyz/mathMS.js
@@ -0,0 +1,23 @@
+// let xyz = require('xyz-core')
+let xyz = require('./../../../xyz-core/index')
+let mathMS = new xyz({
+  selfConf: {
+    name: 'MathMS',
+    host: '127.0.0.1',
+    port: 3333
+  },
+  systemConf: {
+    microservices: [{
+      host: '127.0.0.1',
+      port: 3334
+    }]
+  }
+})
+
+mathMS.register('mul', (payload, response) => {
+  response.send(payload.x * payload.y)
+})
+
+mathMS.register('add', (payload, response) => {
+  response.send(payload.x + payload.y)
+})
---
+++ examples/replicating/math.ms.js
@@ -0,0 +1,21 @@
+// let xyz = require('xyz-core')
+let xyz = require('./../../../xyz-core/index')
+let mathMS = new xyz({
+  selfConf: {
+    allowJoin: true,
+    name: 'MathMS',
+    host: '127.0.0.1',
+    port: 3333
+  },
+  systemConf: {
+    microservices: []
+  }
+})
+
+mathMS.register('mul', (payload, response) => {
+  response.send(payload.x * payload.y)
+})
+
+mathMS.register('add', (payload, response) => {
+  response.send(payload.x + payload.y)
+})
